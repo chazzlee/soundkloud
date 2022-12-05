8bf518402430300b9e9ae73da04733ef073c3c78
chazzlee <chazz@dev.com>
2022-12-05 14:01:32 -0500

    add images for trending playlists section

--- frontend/src/features/landing/components/LandingPage.jsx
+++ frontend/src/features/landing/components/LandingPage.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { LoginModal } from "../../auth/components/LoginModal";
 import { RegisterModal } from "../../auth/components/RegisterModal";
 import styles from "./LandingPage.module.css";
 import "./LandingPage.css";
+import { IoSearch } from "react-icons/io5";
+import { CSSTransition } from "react-transition-group";
 
 const carousels = [
   {
@@ -24,6 +26,85 @@ const carousels = [
   },
 ];
 
+const trendingPlaylists = [
+  {
+    id: 1,
+    title: "Lil Uzi Vert - Just Wanna Rock",
+    user: "Lil Uzi Vert",
+    imageUrl: "https://i1.sndcdn.com/artworks-y1zEjHBDrc2X-0-t500x500.jpg",
+  },
+  {
+    id: 2,
+    title: "Drake, 21 Savage - Rich Flex",
+    user: "octobersveryown",
+    imageUrl: "https://i1.sndcdn.com/artworks-CQla57R6PT2X-0-t500x500.jpg",
+  },
+  {
+    id: 3,
+    title: "OG DAYV, Future - Limoncello",
+    user: "Future",
+    imageUrl: "https://i1.sndcdn.com/artworks-qoNTw55xt95J-0-t500x500.jpg",
+  },
+  {
+    id: 4,
+    title: "Hylan Starr & Wale- If Pretty Was A Person",
+    user: "Hylan Starr",
+    imageUrl:
+      "https://i1.sndcdn.com/artworks-wzj0nrprt6V3ggm3-H3MPTw-t500x500.jpg",
+  },
+  {
+    id: 5,
+    title: "Out thë way",
+    user: "Yeat",
+    imageUrl: "https://i1.sndcdn.com/artworks-NEhRyKEYF3zv-0-t500x500.jpg",
+  },
+  {
+    id: 6,
+    title: "Don't Step",
+    user: "Lil Kizzle",
+    imageUrl: "https://i1.sndcdn.com/artworks-zFxHIaPp9Udl-0-t500x500.jpg",
+  },
+  {
+    id: 7,
+    title: "Patience",
+    user: "Nevi",
+    imageUrl: "https://i1.sndcdn.com/artworks-3nJSJClPP4QJ-0-t500x500.jpg",
+  },
+  {
+    id: 8,
+    title: "Nut Quick",
+    user: "GloRilla",
+    imageUrl: "https://i1.sndcdn.com/artworks-mxhGTjZVy5VJ-0-t500x500.jpg",
+  },
+  {
+    id: 9,
+    title: "Behani & Ne-Yo - Real Man",
+    user: "Behani",
+    imageUrl:
+      "https://i1.sndcdn.com/artworks-zFcobsxrAOQBpRVU-gcxR4w-t500x500.jpg",
+  },
+  {
+    id: 10,
+    title: "One Night Stand",
+    user: "Lonnie",
+    imageUrl:
+      "https://i1.sndcdn.com/artworks-vclDRmT3vLc1oTlo-jCC1kg-t500x500.jpg",
+  },
+  {
+    id: 11,
+    title: "In My Head",
+    user: "Juice WRLD",
+    imageUrl:
+      "https://i1.sndcdn.com/artworks-Gx9raQ9oAzV8FNO0-zLkK8g-t500x500.jpg",
+  },
+  {
+    id: 12,
+    title: "Bad Habit",
+    user: "Steve Lacy",
+    imageUrl: "https://i1.sndcdn.com/artworks-90RGjVBt1Sja-0-t500x500.jpg",
+  },
+];
+
 //TODO: combine modals/add search icon/clean up styles/images
 export function LandingPage() {
   const [loginModalOpen, setLoginModalOpen] = useState(false);
@@ -31,6 +112,8 @@ export function LandingPage() {
   const navigate = useNavigate();
 
   const [carousel, setCarousel] = useState(carousels[0]);
+  const nodeRef = useRef();
+  const [inProp, setInProp] = useState(false);
 
   return (
     <>
@@ -56,52 +139,88 @@ export function LandingPage() {
               </a>
             </div>
           </header>
-          <section
-            className={styles.heroContainer}
-            style={{
-              backgroundImage: `url(${carousel.imageUrl})`,
-            }}
+
+          <CSSTransition
+            nodeRef={nodeRef}
+            in={inProp}
+            timeout={200}
+            classNames="slide"
           >
-            <div className={styles.heroContent}>
-              <h2 className={styles.heroTitle}>{carousel.title}</h2>
-              <p className={styles.heroSubtitle}>{carousel.subTitle}</p>
-            </div>
+            <section
+              ref={nodeRef}
+              className={styles.heroContainer}
+              style={{
+                backgroundImage: `url(${carousel.imageUrl})`,
+              }}
+            >
+              <div className={styles.heroContent}>
+                <h2 className={styles.heroTitle}>{carousel.title}</h2>
+                <p className={styles.heroSubtitle}>{carousel.subTitle}</p>
+              </div>
 
-            <div className={styles.ctaButtonGroup}>
-              <button className={styles.ctaLeft}>Learn more</button>
-              <button className={styles.ctaRight}>
-                Try it free for 30 days
-              </button>
-            </div>
+              <div className={styles.ctaButtonGroup}>
+                {carousel.id === 1 ? (
+                  <>
+                    <button className={styles.ctaLeft}>Learn more</button>
+                    <button className={styles.ctaRight}>
+                      Try it free for 30 days
+                    </button>
+                  </>
+                ) : (
+                  <button className={styles.ctaMiddle}>
+                    Start uploading today
+                  </button>
+                )}
+              </div>
 
-            <div className={styles.heroFooter}>
-              <div
-                className={styles.toggleHeroBtn}
-                style={{
-                  backgroundColor: carousel.id === 1 ? "#fff" : "transparent",
-                }}
-                onClick={() => setCarousel(carousels[0])}
-              />
-              <div
-                className={styles.toggleHeroBtn}
-                style={{
-                  backgroundColor: carousel.id === 2 ? "#fff" : "transparent",
-                }}
-                onClick={() => setCarousel(carousels[1])}
-              />
-            </div>
-          </section>
+              <div className={styles.heroFooter}>
+                <div
+                  className={styles.toggleHeroBtn}
+                  style={{
+                    backgroundColor: carousel.id === 1 ? "#fff" : "transparent",
+                  }}
+                  onClick={() => {
+                    setInProp((prev) => !prev);
+                    setCarousel(carousels[0]);
+                  }}
+                />
+                <div
+                  className={styles.toggleHeroBtn}
+                  style={{
+                    backgroundColor: carousel.id === 2 ? "#fff" : "transparent",
+                  }}
+                  onClick={() => {
+                    setInProp((prev) => !prev);
+                    setCarousel(carousels[1]);
+                  }}
+                />
+              </div>
+            </section>
+          </CSSTransition>
 
           <section className={styles.searchContainer}>
-            <div className={styles.inputBox}>
-              <input
-                type="search"
-                name="search"
-                className="search-input"
-                id="search"
-                placeholder="Search for artists, bands, tracks, podcasts"
-                autoComplete="off"
-              />
+            <div className={styles.inputContainer}>
+              <div className={styles.inputBox}>
+                <input
+                  type="search"
+                  name="search"
+                  className="search-input"
+                  id="search"
+                  placeholder="Search for artists, bands, tracks, podcasts"
+                  autoComplete="off"
+                />
+                <span
+                  style={{
+                    position: "absolute",
+                    fontSize: "22px",
+                    top: 10,
+                    right: 10,
+                    color: "darkgray",
+                  }}
+                >
+                  <IoSearch />
+                </span>
+              </div>
               <span className={styles.or}>or</span>
               <button className={styles.uploadBtn}>Upload your own</button>
             </div>
@@ -113,25 +232,26 @@ export function LandingPage() {
             </h3>
             <div className={styles.center}>
               <div className={styles.trendingList}>
-                {new Array(12)
-                  .fill({ src: "https://via.placeholder.com/180" })
-                  .map((img, i) => (
-                    <div key={i.toString()}>
-                      <img
-                        alt="FIXME"
-                        src={img.src}
-                        height={180}
-                        width={180}
-                        className={styles.trendingListItemImage}
-                      />
-                      <p className={styles.trendingListItemTitle}>
-                        Artist Name - Song Title
-                      </p>
-                      <p className={styles.trendingListItemSubtitle}>
-                        Display Name
-                      </p>
-                    </div>
-                  ))}
+                {trendingPlaylists.map((playlist) => (
+                  <div key={playlist.id}>
+                    <img
+                      alt={playlist.title}
+                      aria-label={playlist.title}
+                      src={playlist.imageUrl}
+                      height={180}
+                      width={180}
+                      className={styles.trendingListItemImage}
+                    />
+                    <p className={styles.trendingListItemTitle}>
+                      {playlist.title.length >= 21
+                        ? `${playlist.title.substring(0, 21)}...`
+                        : playlist.title}
+                    </p>
+                    <p className={styles.trendingListItemSubtitle}>
+                      {playlist.user}
+                    </p>
+                  </div>
+                ))}
               </div>
               <div className={styles.exploreContainer}>
                 <button className={styles.exploreBtn}>
@@ -143,11 +263,16 @@ export function LandingPage() {
 
           <section className={styles.appStoreContainer}>
             <div className={styles.appStoreImageContainer}>
-              <img src="" alt="FIXME" height={"100%"} width={"100%"} />
+              <img
+                src="https://a-v2.sndcdn.com/assets/images/never_stop_listening@1x-9c5264ff.jpg"
+                alt="device-banner"
+                height={"100%"}
+                width={"100%"}
+              />
             </div>
             <div className={styles.appStoreContentContainer}>
               <h3 className={styles.appStoreTitle}>Never stop listening</h3>
-              <div></div>
+              <div className={styles.fancyBorder}></div>
               <p className={styles.appStoreSubtitle}>
                 SoundCloud is available on Web, iOS, Android, Sonos, Chromecast,
                 and Xbox One.
@@ -196,19 +321,19 @@ export function LandingPage() {
 
           <footer className={styles.footer}>
             <div>
-              <a href="">Directory</a>
-              <a href="">About us</a>
-              <a href="">Artist Resources</a>
-              <a href="">Blog</a>
-              <a href="">Jobs</a>
-              <a href="">Developers</a>
-              <a href="">Help</a>
-              <a href="">Legal</a>
-              <a href="">Privacy</a>
-              <a href="">Cookie Policy</a>
-              <a href="">Cookie Manager</a>
-              <a href="">Imprint</a>
-              <a href="">Charts</a>
+              <a href="#directory">Directory</a>
+              <a href="#about-us">About us</a>
+              <a href="#artist-resources">Artist Resources</a>
+              <a href="#blog">Blog</a>
+              <a href="#jobs">Jobs</a>
+              <a href="#developers">Developers</a>
+              <a href="#help">Help</a>
+              <a href="#legal">Legal</a>
+              <a href="#privacy">Privacy</a>
+              <a href="#cookie-policy">Cookie Policy</a>
+              <a href="#cookie-manager">Cookie Manager</a>
+              <a href="#imprint">Imprint</a>
+              <a href="#charts  ">Charts</a>
             </div>
             <button className={styles.languageBtn}>
               Language: <span style={{ color: "#333" }}>English (US)</span>
