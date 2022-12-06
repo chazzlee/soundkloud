689df1f661a9c7f0a68ddd98cf0dd758e32db737
chazzlee <chazz@dev.com>
2022-12-05 20:41:31 -0500

    add error message for login modal

--- frontend/src/features/auth/components/LoginModal.jsx
+++ frontend/src/features/auth/components/LoginModal.jsx
@@ -12,7 +12,7 @@ import "./LoginModal.css";
 //TODO: validations - form state errors etc...
 export function LoginModal({ onClose, onSuccess }) {
   const [formValues, setFormValues] = useState({
-    email: "jdoe@test.com",
+    email: "test@test.com",
     password: "password",
   });
 
@@ -23,18 +23,21 @@ export function LoginModal({ onClose, onSuccess }) {
   const [error, setError] = useState(null);
   const [step, setStep] = useState("email");
 
+  const isEmailStep = step === "email";
+  const isPasswordStep = step === "password";
+  const isCreateNewStep = step === "create";
+
   const dispatch = useDispatch();
 
   /**
    * @param {React.ChangeEvent<HTMLInputElement>} event
    */
   const handleInputChange = (event) => {
-    if (formState.submitted) {
-      setError(null);
-    }
-
     //TODO:! show error message again after first empty submit and second empty touch
 
+    // if (formState.submitted) {
+    //   setError(null);
+    // }
     setFormValues((prev) => ({
       ...prev,
       [event.target.name]: event.target.value,
@@ -50,6 +53,7 @@ export function LoginModal({ onClose, onSuccess }) {
     setFormState((prev) => ({ ...prev, submitted: true }));
     if (!formValues.email) {
       setError("Enter a valid email address or profile url.");
+      return;
     }
     //-============
     const response = await csrfFetch("/api/user", {
@@ -60,7 +64,7 @@ export function LoginModal({ onClose, onSuccess }) {
     if (data.success) {
       setStep("password");
     } else {
-      console.log("NO USER"); //TODO: set to register step instead
+      setStep("create"); //TODO: set to register step instead
     }
   };
 
@@ -83,126 +87,249 @@ export function LoginModal({ onClose, onSuccess }) {
       .catch((err) => console.error("my err", err));
   };
 
-  return (
-    <Modal onClose={onClose}>
+  let currentStep = null;
+  if (isEmailStep) {
+    currentStep = (
       <div className={styles.modalForm}>
-        {step === "email" ? (
-          <>
-            <button onClick={onClose} className={styles.closeBtn} title="Close">
-              <IoMdClose />
-            </button>
-            <div className={styles.socialButtonGroup}>
-              <button className="social-login-btn facebook">
-                <img
-                  src="https://secure.sndcdn.com/assets/facebook-8d9809.png"
-                  alt=""
-                  height="15px"
-                  width="15px"
-                  style={{ marginRight: "0.5rem" }}
-                />
-                <div>Continue with Facebook</div>
-              </button>
-              <button className="social-login-btn google">
-                <img
-                  src="https://secure.sndcdn.com/assets/google-a6c367.svg"
-                  alt=""
-                  height="15px"
-                  width="15px"
-                  style={{ marginRight: "0.5rem" }}
-                />
-                <div>Continue with Google</div>
-              </button>
-              <button className="social-login-btn apple">
-                <img
-                  src="https://secure.sndcdn.com/assets/apple-0a88d2.svg"
-                  alt=""
-                  height="28px"
-                  width="28px"
-                  style={{ marginRight: "0.3rem", display: "block" }}
-                />
-                <div>Continue with Apple</div>
-              </button>
-            </div>
-            <div className={styles.or}>or</div>
-            <form onSubmit={handleNextStep} noValidate>
-              <input
-                className={styles.input}
-                type="email"
-                name="email"
-                placeholder="Your email address or profile URL"
-                value={formValues.email}
-                onChange={handleInputChange}
-              />
-              {error ? error : null}
-              <button type="submit" className={styles.continueBtn}>
-                Continue
-              </button>
-            </form>
-            <div className={styles.modalFooter}>
-              <a href="#help" className={styles.needHelpLink}>
-                Need help?
-              </a>
-              <p className={styles.privacyPolicy}>
-                When registering, you agree that we may use your provided data
-                for the registration and to send you notifications on our
-                products and services. You can unsubscribe from notifications at
-                any time in your settings. For additional info please refer to
-                our{" "}
-                <a href="#privacy-policy" style={{ textDecoration: "none" }}>
-                  Privacy Policy
-                </a>
-                .
-              </p>
-            </div>
-          </>
-        ) : (
-          <>
-            <button onClick={onClose} className={styles.closeBtn} title="Close">
-              <IoMdClose />
-            </button>
-            <h3 className={styles.welcomeTitle}>Welcome back!</h3>
-            <form onSubmit={handleLogin} noValidate>
-              <div
-                className={styles.input}
-                style={{
-                  display: "inline-flex",
-                  alignItems: "center",
-                  marginBottom: "14px",
-                }}
-              >
-                <IoCaretBack
-                  style={{ marginRight: "9px" }}
-                  onClick={() => setStep("email")}
-                />
-                <span>{formValues.email}</span>
-              </div>
-              <input
-                className={styles.input}
-                type="password"
-                name="password"
-                placeholder="Your Password"
-                value={formValues.password}
-                onChange={handleInputChange}
-              />
-              {error ? error : null}
-              <button type="submit" className={styles.continueBtn}>
-                Sign In
-              </button>
-            </form>
-            <p
-              style={{
-                textAlign: "center",
-                fontSize: "12px",
-                color: "#044dd2",
-                margin: 0,
-                paddingBottom: "80px",
-              }}
-            >
-              Don't know your password?
-            </p>
-          </>
-        )}
+        <button onClick={onClose} className={styles.closeBtn} title="Close">
+          <IoMdClose />
+        </button>
+        <div className={styles.socialButtonGroup}>
+          <button className="social-login-btn facebook">
+            <img
+              src="https://secure.sndcdn.com/assets/facebook-8d9809.png"
+              alt=""
+              height="15px"
+              width="15px"
+              style={{ marginRight: "0.5rem" }}
+            />
+            <div>Continue with Facebook</div>
+          </button>
+          <button className="social-login-btn google">
+            <img
+              src="https://secure.sndcdn.com/assets/google-a6c367.svg"
+              alt=""
+              height="15px"
+              width="15px"
+              style={{ marginRight: "0.5rem" }}
+            />
+            <div>Continue with Google</div>
+          </button>
+          <button className="social-login-btn apple">
+            <img
+              src="https://secure.sndcdn.com/assets/apple-0a88d2.svg"
+              alt=""
+              height="28px"
+              width="28px"
+              style={{ marginRight: "0.3rem", display: "block" }}
+            />
+            <div>Continue with Apple</div>
+          </button>
+        </div>
+        <div className={styles.or}>or</div>
+        <form onSubmit={handleNextStep} noValidate>
+          <input
+            className={`${styles.input} ${error && styles.invalid}`}
+            type="email"
+            name="email"
+            placeholder="Your email address or profile URL"
+            value={formValues.email}
+            onChange={handleInputChange}
+          />
+          {error ? <p className={styles.invalidMessage}>{error}</p> : null}
+          <button type="submit" className={styles.continueBtn}>
+            Continue
+          </button>
+        </form>
+        <div className={styles.modalFooter}>
+          <a href="#help" className={styles.needHelpLink}>
+            Need help?
+          </a>
+          <p className={styles.privacyPolicy}>
+            When registering, you agree that we may use your provided data for
+            the registration and to send you notifications on our products and
+            services. You can unsubscribe from notifications at any time in your
+            settings. For additional info please refer to our{" "}
+            <a href="#privacy-policy" style={{ textDecoration: "none" }}>
+              Privacy Policy
+            </a>
+            .
+          </p>
+        </div>
       </div>
-    </Modal>
-  );
+    );
+  } else if (isPasswordStep) {
+    currentStep = (
+      <div className={styles.modalForm}>
+        <button onClick={onClose} className={styles.closeBtn} title="Close">
+          <IoMdClose />
+        </button>
+        <h3 className={styles.welcomeTitle}>Welcome back!</h3>
+        <form onSubmit={handleLogin} noValidate>
+          <div
+            className={styles.input}
+            style={{
+              display: "inline-flex",
+              alignItems: "center",
+              marginBottom: "14px",
+            }}
+          >
+            <IoCaretBack
+              style={{ marginRight: "9px" }}
+              onClick={() => setStep("email")}
+            />
+            <span>{formValues.email}</span>
+          </div>
+          <input
+            className={styles.input}
+            type="password"
+            name="password"
+            placeholder="Your Password"
+            value={formValues.password}
+            onChange={handleInputChange}
+          />
+          {error ? error : null}
+          <button type="submit" className={styles.continueBtn}>
+            Sign In
+          </button>
+        </form>
+        <p
+          style={{
+            textAlign: "center",
+            fontSize: "12px",
+            color: "#044dd2",
+            margin: 0,
+            paddingBottom: "80px",
+          }}
+        >
+          Don't know your password?
+        </p>
+      </div>
+    );
+  }
+
+  return <Modal onClose={onClose}>{currentStep}</Modal>;
+
+  // return (
+  //   <Modal onClose={onClose}>
+  //     <div className={styles.modalForm}>
+  //       {step === "email" ? (
+  //         <>
+  //           <button onClick={onClose} className={styles.closeBtn} title="Close">
+  //             <IoMdClose />
+  //           </button>
+  //           <div className={styles.socialButtonGroup}>
+  //             <button className="social-login-btn facebook">
+  //               <img
+  //                 src="https://secure.sndcdn.com/assets/facebook-8d9809.png"
+  //                 alt=""
+  //                 height="15px"
+  //                 width="15px"
+  //                 style={{ marginRight: "0.5rem" }}
+  //               />
+  //               <div>Continue with Facebook</div>
+  //             </button>
+  //             <button className="social-login-btn google">
+  //               <img
+  //                 src="https://secure.sndcdn.com/assets/google-a6c367.svg"
+  //                 alt=""
+  //                 height="15px"
+  //                 width="15px"
+  //                 style={{ marginRight: "0.5rem" }}
+  //               />
+  //               <div>Continue with Google</div>
+  //             </button>
+  //             <button className="social-login-btn apple">
+  //               <img
+  //                 src="https://secure.sndcdn.com/assets/apple-0a88d2.svg"
+  //                 alt=""
+  //                 height="28px"
+  //                 width="28px"
+  //                 style={{ marginRight: "0.3rem", display: "block" }}
+  //               />
+  //               <div>Continue with Apple</div>
+  //             </button>
+  //           </div>
+  //           <div className={styles.or}>or</div>
+  //           <form onSubmit={handleNextStep} noValidate>
+  //             <input
+  //               className={`${styles.input} ${error && styles.invalid}`}
+  //               type="email"
+  //               name="email"
+  //               placeholder="Your email address or profile URL"
+  //               value={formValues.email}
+  //               onChange={handleInputChange}
+  //             />
+  //             {error ? <p className={styles.invalidMessage}>{error}</p> : null}
+  //             <button type="submit" className={styles.continueBtn}>
+  //               Continue
+  //             </button>
+  //           </form>
+  //           <div className={styles.modalFooter}>
+  //             <a href="#help" className={styles.needHelpLink}>
+  //               Need help?
+  //             </a>
+  //             <p className={styles.privacyPolicy}>
+  //               When registering, you agree that we may use your provided data
+  //               for the registration and to send you notifications on our
+  //               products and services. You can unsubscribe from notifications at
+  //               any time in your settings. For additional info please refer to
+  //               our{" "}
+  //               <a href="#privacy-policy" style={{ textDecoration: "none" }}>
+  //                 Privacy Policy
+  //               </a>
+  //               .
+  //             </p>
+  //           </div>
+  //         </>
+  //       ) : (
+  //         <>
+  //           <button onClick={onClose} className={styles.closeBtn} title="Close">
+  //             <IoMdClose />
+  //           </button>
+  //           <h3 className={styles.welcomeTitle}>Welcome back!</h3>
+  //           <form onSubmit={handleLogin} noValidate>
+  //             <div
+  //               className={styles.input}
+  //               style={{
+  //                 display: "inline-flex",
+  //                 alignItems: "center",
+  //                 marginBottom: "14px",
+  //               }}
+  //             >
+  //               <IoCaretBack
+  //                 style={{ marginRight: "9px" }}
+  //                 onClick={() => setStep("email")}
+  //               />
+  //               <span>{formValues.email}</span>
+  //             </div>
+  //             <input
+  //               className={styles.input}
+  //               type="password"
+  //               name="password"
+  //               placeholder="Your Password"
+  //               value={formValues.password}
+  //               onChange={handleInputChange}
+  //             />
+  //             {error ? error : null}
+  //             <button type="submit" className={styles.continueBtn}>
+  //               Sign In
+  //             </button>
+  //           </form>
+  //           <p
+  //             style={{
+  //               textAlign: "center",
+  //               fontSize: "12px",
+  //               color: "#044dd2",
+  //               margin: 0,
+  //               paddingBottom: "80px",
+  //             }}
+  //           >
+  //             Don't know your password?
+  //           </p>
+  //         </>
+  //       )}
+  //     </div>
+  //   </Modal>
+  // );
 }
