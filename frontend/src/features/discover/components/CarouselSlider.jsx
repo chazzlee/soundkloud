3931987c0a846165cd6e58ea6e5bf62aabc7aadf
chazzlee <chazz@dev.com>
2022-12-10 14:24:55 -0500

    hide scrollbar for main column

---
+++ frontend/src/features/discover/components/CarouselSlider.jsx
@@ -0,0 +1,78 @@
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import {
+  ButtonBack,
+  ButtonNext,
+  CarouselProvider,
+  Slide,
+  Slider,
+  Image,
+} from "pure-react-carousel";
+import styles from "./DiscoverPage.module.css";
+import { useState } from "react";
+
+const VISIBLE_SLIDES = 4;
+
+export function CarouselSlider({ title, slides }) {
+  const totalRows = slides.length / VISIBLE_SLIDES;
+  const firstRowIndex = 0;
+  const lastRowIndex = totalRows - 1;
+
+  const [slidesIndex, setSlidesIndex] = useState(firstRowIndex);
+
+  return (
+    <div
+      className={styles.carouselContainer}
+      style={{ borderBottom: "1px solid var(--bg-light)" }}
+    >
+      <h2 className={styles.carouselHeading}>{title}</h2>
+      <CarouselProvider
+        naturalSlideWidth={180}
+        naturalSlideHeight={240}
+        totalSlides={slides.length}
+        dragEnabled={false}
+        visibleSlides={4.25}
+        step={4}
+        className={styles.relative}
+      >
+        {slidesIndex > firstRowIndex && (
+          <ButtonBack
+            className={styles.carouselControl}
+            style={{ left: -12 }}
+            onClick={() => setSlidesIndex((prev) => prev - 1)}
+          >
+            <RxCaretLeft className={styles.arrowIcon} />
+          </ButtonBack>
+        )}
+        <Slider>
+          {slides.map((slide, index) => (
+            <Slide key={slide.id} index={index}>
+              <div>
+                <Image
+                  src="https://www.metalkingdom.net/album-cover-artwork/2018/02/3/3062-Nile-Annihilation-of-the-Wicked.jpg"
+                  style={{
+                    objectFit: "cover",
+                    width: "90%",
+                    height: "90%",
+                  }}
+                />
+              </div>
+              <div style={{ marginTop: "6px" }}>
+                <p className={styles.title}>{slide.title}</p>
+                <p className={styles.subcaption}>{"Related tracks"}</p>
+              </div>
+            </Slide>
+          ))}
+        </Slider>
+        {slidesIndex < lastRowIndex && (
+          <ButtonNext
+            className={styles.carouselControl}
+            style={{ right: -12 }}
+            onClick={() => setSlidesIndex((prev) => prev + 1)}
+          >
+            <RxCaretRight className={styles.arrowIcon} />
+          </ButtonNext>
+        )}
+      </CarouselProvider>
+    </div>
+  );
+}
--- frontend/src/features/discover/components/DiscoverPage.jsx
+++ frontend/src/features/discover/components/DiscoverPage.jsx
@@ -1,11 +1,3 @@
-import {
-  ButtonBack,
-  ButtonNext,
-  CarouselProvider,
-  Slide,
-  Slider,
-  Image,
-} from "pure-react-carousel";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -15,12 +7,8 @@ import {
   selectIsTracksLoading,
   selectTracksError,
 } from "../../tracks/store";
-import { CarouselList } from "./CarouselList";
 import styles from "./DiscoverPage.module.css";
-import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
-import { useState } from "react";
-
-const VISIBLE_SLIDES = 4;
+import { CarouselSlider } from "./CarouselSlider";
 
 export function DiscoverPage() {
   const dispatch = useDispatch();
@@ -29,12 +17,7 @@ export function DiscoverPage() {
   const error = useSelector(selectTracksError);
   const tracks = useSelector(selectAllTracks);
 
-  const sampleSlides = tracks.slice(0, 16);
-  const totalRows = sampleSlides.length / VISIBLE_SLIDES;
-  const firstRowIndex = 0;
-  const lastRowIndex = totalRows - 1;
-
-  const [slidesIndex, setSlidesIndex] = useState(firstRowIndex);
+  const test = [...tracks];
 
   useEffect(() => {
     if (!loaded) {
@@ -58,59 +41,27 @@ export function DiscoverPage() {
     <div className="full-page">
       <main className={`page-container ${styles.innerContainer}`}>
         <div className={styles.columnMain}>
-          <div
-            className={styles.carouselContainer}
-            style={{ borderBottom: "1px solid var(--bg-light)" }}
-          >
-            <h2 className={styles.carouselHeading}>More of what you like</h2>
-            <CarouselProvider
-              naturalSlideWidth={180}
-              naturalSlideHeight={240}
-              totalSlides={sampleSlides.length}
-              dragEnabled={false}
-              visibleSlides={4.2}
-              step={4}
-            >
-              {slidesIndex > firstRowIndex && (
-                <ButtonBack
-                  className={styles.carouselControl}
-                  style={{ left: 12 }}
-                  onClick={() => setSlidesIndex((prev) => prev - 1)}
-                >
-                  <RxCaretLeft className={styles.arrowIcon} />
-                </ButtonBack>
-              )}
-              <Slider>
-                {sampleSlides.map((slide, index) => (
-                  <Slide key={slide.id} index={index}>
-                    <div>
-                      <Image
-                        src="https://www.metalkingdom.net/album-cover-artwork/2018/02/3/3062-Nile-Annihilation-of-the-Wicked.jpg"
-                        style={{
-                          objectFit: "cover",
-                          width: "90%",
-                          height: "90%",
-                        }}
-                      />
-                    </div>
-                    <div>
-                      <p className={styles.title}>{slide.title}</p>
-                      <p className={styles.subcaption}>{"Related tracks"}</p>
-                    </div>
-                  </Slide>
-                ))}
-              </Slider>
-              {slidesIndex < lastRowIndex && (
-                <ButtonNext
-                  className={styles.carouselControl}
-                  style={{ right: 375 }}
-                  onClick={() => setSlidesIndex((prev) => prev + 1)}
-                >
-                  <RxCaretRight className={styles.arrowIcon} />
-                </ButtonNext>
-              )}
-            </CarouselProvider>
-          </div>
+          <CarouselSlider
+            title="More of what you like"
+            slides={tracks.slice(0, 16)}
+          />
+          <CarouselSlider title="Recently Played" slides={test.slice(0, 16)} />
+          <CarouselSlider title="Next Wav Miami" slides={test.slice(0, 16)} />
+
+          <div></div>
+          {/* TODO: Daily Drops?? */}
+
+          <CarouselSlider title="Fresh Pressed" slides={test.slice(0, 16)} />
+          <CarouselSlider title="Charts: Top 50" slides={test.slice(0, 16)} />
+          <CarouselSlider
+            title="Charts: New & hot"
+            slides={test.slice(0, 16)}
+          />
+          <CarouselSlider title="Rock & Metal" slides={test.slice(0, 16)} />
+          <CarouselSlider title="New Music Now" slides={test.slice(0, 16)} />
+          <CarouselSlider title="EDM" slides={test.slice(0, 16)} />
+          <CarouselSlider title="Emo Rap & More" slides={test.slice(0, 16)} />
+          <CarouselSlider title="Chill Tunes" slides={test.slice(0, 16)} />
         </div>
         <div className={styles.columnAside}>
           <p>ASIDE</p>
