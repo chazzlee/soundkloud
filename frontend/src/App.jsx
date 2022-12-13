b0c3b32f236329ffbe24be7aefec0238417b4d13
chazzlee <chazz@dev.com>
2022-12-13 12:23:52 -0500

    render fixed audio player

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { HeadProvider } from "react-head";
 import { Outlet, useLocation } from "react-router-dom";
 import { TopNavigation } from "./components/TopNavigation";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 
 //TODO: figure out react-head
 function App() {
@@ -11,6 +13,21 @@ function App() {
     <>
       {!isLandingPage ? <TopNavigation /> : null}
       <Outlet />
+      <div
+        style={{
+          position: "fixed",
+          bottom: 0,
+          width: "100%",
+          zIndex: 2,
+          padding: 0,
+          border: "1px solid #cecece",
+        }}
+      >
+        <AudioPlayer
+          style={{ backgroundColor: "#f2f2f2" }}
+          layout="horizontal"
+        />
+      </div>
     </>
   );
 }
